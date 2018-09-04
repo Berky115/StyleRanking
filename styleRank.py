516e88e1394818171884013ed41d7052336715f9
Andrew Facchiano <an4d2ea@L-SFF0SZHTDD-M.local>
2018-09-03 19:12:49 -0700

    Drew - Adding GameLoop object. Creates new thread to progression tracking

--- styleRank.py
+++ styleRank.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 class Rank(object):
     def __init__(self, name, glyph, priority, activation_sound, progression_count, orb_multiplier):
         self.name = name
@@ -18,40 +21,80 @@ class Ranks(object):
         self.SS = Rank("Sexy Style!", "SS", 6, "SSensational.mp3" , 3000, 2.5)
         self.SSS = Rank("Sweet Son of Sparda!", "SSS", 7, "SSSensational.mp3" , 4000, 3)
 
+        self.rank_list = {
+            0 : self.NONE,
+            1 : self.D,
+            2 : self.C,
+            3 : self.B,
+            4 : self.A,
+            5 : self.S,
+            6 : self.SS,
+            7 : self.SSS
+        }
+
+    def getRank(self, priority):
+        return self.rank_list[priority]
 
 class RankService(object):
-    def __init__(self):
-        self.rank_List = [
-            Rank("","",0,"",1000,1) ,
-            Rank("Damnation", "D", 1, "D-d-d-damn!.mp3" , 1000, 1.25),
-            Rank("Crazy!", "C", 2, "CRAZY.mp3" , 1300, 1.5),
-            Rank("Bravo!", "B", 3, "Bravo.mp3" , 1500, 1.75),
-            Rank("Anarchy!", "A", 4, "Anarchy.mp3" , 1700, 2),
-            Rank("Sensational!", "S", 5, "Sensational.mp3" , 2000, 2.25),
-            Rank("Sexy Style!", "SS", 6, "SSensational.mp3" , 3000, 2.5),
-            Rank("Sweet Son of Sparda!", "SSS", 7, "SSSensational.mp3" , 4000, 3)
-           ]
-        self.current_rank = self.rank_List[0]
+    def __init__(self, rank_list):
+        self.rank_List = rank_list
+        self.current_rank = self.rank_List.getRank(0)
         self.max_progression = self.current_rank.progression_count
-        self.current_progression = 0
+        self.current_progression = 10
         self.orb_multiplier = self.current_rank.orb_multiplier
+        self.decreaseValue = 1;
+
+    def polling_value(self):
+        #Debug:
+        print(self.current_progression , " / " , self.max_progression )
+        print(self.current_rank.name)
+
+        if self.current_rank.priority > 0:
+            self.current_progression -= self.decreaseValue;
+
+        if self.current_progression < 0 and self.current_rank.priority > 0:
+            self.rankDown()
+
+        if self.current_progression > self.max_progression and self.current_rank.priority <7:
+            self.rankUp()
+
+
     def applyPoints(self, new_points):
         self.current_progression += new_points * self.orb_multiplier
         if self.current_progression > self.max_progression:
             self.rankUp()
         elif self.current_progression < 0:
             self.rankDown()
+
     def rankUp(self):
         self.current_progression = self.current_progression - self.max_progression
-        self.current_rank = self.rank_List[self.current_rank.priority + 1]
+        self.current_rank = self.rank_List.getRank(self.current_rank.priority + 1)
         self.max_progression = self.current_rank.progression_count
         self.orb_multiplier = self.current_rank.orb_multiplier
+        #Debug:
+        print(self.current_rank.activation_sound)
+
     def rankDown(self):
-        self.current_rank = self.rank_List[self.current_rank.priority - 1]
+        self.current_rank = self.rank_List.getRank(self.current_rank.priority - 1)
         self.max_progression = self.current_rank.progression_count
         self.orb_multiplier = self.current_rank.orb_multiplier
         self.current_progression = self.max_progression // 2
+        #Debug:
+        print(self.current_rank.activation_sound)
+
     def setRank(self, rank):
         self.current_rank = rank
         self.max_progression = rank.progression_count
         self.orb_multiplier = rank.orb_multiplier
+
+class GameLoop(threading.Thread):
+    def __init__(self,rank_service):
+        self.rank_service = rank_service
+        self.rank_service_running = True
+        threading.Thread.__init__(self)
+        self.start()
+
+    def run(self):
+        while self.rank_service_running:
+            time.sleep(1)
+            self.rank_service.polling_value()
--- styleRankTest.ut.py
+++ styleRankTest.ut.py
@@ -1,10 +1,11 @@
 import unittest
+import threading
+import time
 
-from styleRank import Rank, Ranks, RankService
+from styleRank import Rank, Ranks, RankService, GameLoop
 
 class TestRankFunctionality(unittest.TestCase):
     def test_can_make_rank(self):
-        #name, glyph, priority, activation_sound, progression_count, orb_multiplier
         rank = Rank("Damnation", "D", 1, "D-d-d-damn!.mp3" , 1000, .1)
         self.assertEquals(rank.name == "Damnation", True)
         self.assertEquals(rank.glyph == "D", True)
@@ -16,18 +17,29 @@ class TestRankFunctionality(unittest.TestCase):
 class TestRankService(unittest.TestCase):
     def test_rank_up(self):
         test_rank = Rank("Damnation", "D", 1, "D-d-d-damn!.mp3" , 1000, .1)
-        rank_service = RankService()
+        rank_service = RankService(Ranks())
         rank_service.current_progression = 999
         rank_service.applyPoints(100)
         self.assertEquals(rank_service.current_rank.name == test_rank.name,True)
+
     def test_rank_down(self):
-        rank_service = RankService()
+        rank_service = RankService(Ranks())
         ranks = Ranks()
         rank_service.setRank(ranks.C)
         rank_service.current_progression = 1
         rank_service.applyPoints(-100)
         self.assertEquals(rank_service.current_rank.name == ranks.D.name,True)
 
+
+class TestThreadBehavior(unittest.TestCase):
+    def test_threading_behavior(self):
+        gameLoop = GameLoop(RankService(Ranks()))
+        gameLoop.rank_service.decreaseValue = 50
+        ranks = Ranks()
+        gameLoop.rank_service.setRank(ranks.S)
+        time.sleep(5)
+        self.assertEquals(gameLoop.rank_service.current_rank.name != ranks.S.name,True)
+        gameLoop.rank_service_running = False
 
 if __name__ == '__main__':
     unittest.main()
